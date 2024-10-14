dc05d2f4b9015c3229f5fe9ca19e493abef040bd
kaaduarte <programacaoduarte@gmail.com>
2024-10-14 10:23:14 -0300

    5

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const
+const botaoSomar = document.getElementById("somar");
+const botaoSubtrair = document.getElementById("subtrair");
+const botaoMultiplicar = document.getElementById("multiplicar");
+const botaoDividir = document.getElementById("dividir");
+const igual = document.getElementById("igual");
 
 function somar {
 
